35b37139b1d97ce7a3c1633e64797418a1b66b0f
kenny3755 <kenny375@naver.com>
2025-10-30 15:17:08 +0900

    Update hw06_Frag.glsl

--- hw6/hw06_Frag.glsl
+++ hw6/hw06_Frag.glsl
@@ -1,14 +1,12 @@
 #version 300 es
 precision mediump float;
 
-// 정점 셰이더에서 전달받은 텍스처 좌표
 in vec2 v_texCoord;
-// 텍스처 샘플러 유닛 (sunrise.jpg가 바인딩될 곳)
+
 uniform sampler2D u_sampler;
 
 out vec4 outColor;
 
 void main() {
-    // 텍스처에서 v_texCoord 위치의 색상을 읽어와 픽셀 색상으로 출력
-    outColor = texture(u_sampler, v_texCoord);
+    outColor = texture(u_sampler, vec2(v_texCoord.x, 1.0 - v_texCoord.y));
 }
